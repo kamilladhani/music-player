fea270b48eaa33171ad5eeb90aff7d2428192dd1
Kamil Ladhani <kamilladhani@gmail.com>
2023-06-06 14:51:05 -0700

    Added a basic scrubber

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Home from './pages/Home';
 
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
+      <GestureHandlerRootView style={styles.container}>
         <Home />
         <StatusBar style="auto" />
-      </View>
+      </GestureHandlerRootView>
     </PaperProvider>
   );
 }
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useEffect, useRef } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { SONGS } from '../sampledata/songlist.json'
 import SongDetails from '../components/SongDetails';
 import SongControls from '../components/SongControls';
+import Scrubber from 'react-native-scrubber'
+import { useTheme } from 'react-native-paper';
 
 export interface Song {
   title: string
@@ -13,16 +15,35 @@ export interface Song {
 export default function Home() {
   const [songs, setSongs] = useState<Song[]>([]);
   const [currentSong, setCurrentSong] = useState<number>(0);
-  const [isPlay, setIsPlay] = useState<boolean>(false);
+  const [isPlay, setIsPlay] = useState<boolean>(true);
   const [isRepeat, setIsRepeat] = useState<boolean>(false);
   const [isShuffle, setIsShuffle] = useState<boolean>(false);
+  const [scrubberValue, setScrubberValue] = useState<number>(0);
+  const intervalRef = useRef<any>()
 
+  const theme = useTheme()
+
+  const playSong = () => {
+    clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => {
+      if (isPlay) {
+        setScrubberValue(scrubberValue => scrubberValue+1);
+      }
+    }, 1000);
+  }
+
+  useEffect(() => {
+    isPlay ? playSong() : clearInterval(intervalRef.current)
+  }, [isPlay])
 
   useMemo(() => {
     // Call API to get songs
     setSongs(SONGS)
   }, [SONGS])
   
+  const onSlidingComplete = () => {
+    return null
+  }
 
   return (
     <View style={styles.container}>
@@ -33,6 +54,16 @@ export default function Home() {
         isRepeat={isRepeat}
         isShuffle={isShuffle}
       />
+      {songs.length > 0 ? 
+        <Scrubber 
+          value={scrubberValue} 
+          onSlidingComplete={onSlidingComplete}
+          totalDuration={songs[currentSong].duration}
+          trackColor={theme.colors.surfaceDisabled}
+          scrubbedColor={theme.colors.primary}
+        /> 
+        : null
+      }
     </View>
   );
 }
